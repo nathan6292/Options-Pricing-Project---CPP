2cdcbd29debf71c7a98372bfa5ee37629c05763f
Julien031103 <julien.gilles@edu.devinci.fr>
2024-11-10 12:04:09 +0100

    Create EuropeanVanillaOption.h

---
+++ Julien/EuropeanVanillaOption.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<iostream>
+#include"Option.h"
+#include"optionType.h"
+using namespace std;
+
+class EuropeanVanillaOption : public Option
+{
+public:
+	EuropeanVanillaOption(double, double); //pas besoin de mettre strike et expiry direct là
+	virtual optionType GetOptionType()const=0;
+	double payoff(double) const override; //pas besoin de réécrire virtual (déjà écrit dans classe mère)
+	friend class BlackScholesPricer;
+private:
+	double _strike;
+protected:
+	optionType _type; //si pas en protected, type pas reconnu dans classes calloption et putoption
+};
