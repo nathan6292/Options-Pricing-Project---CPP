c4602ea6086e5c171c6e54b48b11ea3767b0649e
nathan6292 <nathan.fleury@edu.devinci.fr>
2024-11-24 14:54:45 +0100

    adding Alix update

--- CRRPricer.cpp
+++ CRRPricer.cpp
@@ -1,15 +1,20 @@
 #include "CRRPricer.h"
 
 double power(double x, int n) {
-	if (n == 0) return 1;
-	double half = power(x, n / 2);
-	if (n % 2 == 0) return half * half;
-	else return x * half * half;
+	double result = 1.0;
+	while (n > 0) {
+		if (n % 2 == 1) result *= x;
+		x *= x;
+		n /= 2;
+	}
+	return result;
 }
 
-CRRPricer::CRRPricer(Option* option, int depth, double asset_price, double up, double down, double interest_rate){
+// Constructor with up, down and interest rate
+CRRPricer::CRRPricer(Option* option, int depth, double asset_price, double up, double down, double interest_rate) {
 
-	if ((*option).isAsianOption()) {
+	// Check if the option is an Asian option to throw an exception
+	if (option->isAsianOption()) {
 		throw std::invalid_argument("The option is an Asian Option!");
 	}
 
@@ -22,55 +27,57 @@ CRRPricer::CRRPricer(Option* option, int depth, double asset_price, double up, d
 	this->up = up;
 	this->down = down;
 	this->interest_rate = interest_rate;
-	
-	tree = BinaryTree<double>(depth);
-	tree_bool = BinaryTree<bool>(depth);
 
-	for (int i = 0; i < depth; i++) {
-		double final_price = asset_price * power(1+up, i) * power(1+down, depth-i-1);
-		tree.setNode(depth-1,i,(*option).payoff(final_price));
-	}
+	tree.setDepth(depth); // Initialise the depth of the tree
+	tree_bool.setDepth(depth);
 }
-
+// Constructor with r and volatility that compute up, down and interest rate
 CRRPricer::CRRPricer(Option* option, int depth, double asset_price, double r, double volatility) {
 
-	if ((*option).isAsianOption()) {
+	if (option->isAsianOption()) {
 		throw std::invalid_argument("The option is an Asian Option!");
 	}
 
 	this->option = option;
 	this->depth = depth;
 	this->asset_price = asset_price;
-	this->up = std::exp((r+ (volatility * volatility) / 2) * ((*option).getExpiry() / depth) + volatility * std::sqrt((*option).getExpiry() / depth)) - 1;
-	this->down = std::exp((r + (volatility * volatility) / 2) * ((*option).getExpiry() / depth) - volatility * std::sqrt((*option).getExpiry() / depth)) - 1;
-	this->interest_rate = std::exp(r*((*option).getExpiry()/ depth))-1;
+	//U = exp^[(r+σ*σ/2)*h+σ√h]− 1
+	this->up = std::exp((r + (volatility * volatility) / 2) * (option->getExpiry() / depth) + volatility * std::sqrt(option->getExpiry() / depth)) - 1;
+	//D = exp^[(r+σ*σ/2)*h-σ√h]− 1
+	this->down = std::exp((r + (volatility * volatility) / 2) * (option->getExpiry() / depth) - volatility * std::sqrt(option->getExpiry() / depth)) - 1;
+	//R = exp^(rh − 1)
+	this->interest_rate = std::exp(r * (option->getExpiry() / depth)) - 1;
 
 	if (interest_rate >= up || interest_rate <= down)
 		throw std::invalid_argument("The model admits Arbitrage!");
 
-	tree = BinaryTree<double>(depth);
-	tree_bool = BinaryTree<bool>(depth);
-
-	for (int i = 0; i < depth; i++) {
-		double final_price = asset_price * power(1 + up, i) * power(1 + down, depth - i - 1);
-		tree.setNode(depth - 1, i, (*option).payoff(final_price));
-	}
+	tree.setDepth(depth); // Initialise the depth of the tree
+	tree_bool.setDepth(depth);
 }
 
+
 void CRRPricer::compute() {
+	// Initialise values at expiry level N
+	for (int i = 0; i <= depth; ++i) {
+		double final_price = asset_price * std::pow(1 + up, i) * std::pow(1 + down, depth - i); //we simply use the formula S(N,i)=S0*U^i*D^(N-i)
+		tree.setNode(depth, i, option->payoff(final_price));
+		tree_bool.setNode(depth, i, false); // On expiry, there's no question of early exercise
+	}
+	// Induction to calculate values at previous levels
 	double q = (interest_rate - down) / (up - down);
-	if (AmericanOption* americanoption = dynamic_cast<AmericanOption*>(option)) {
-		for (int i = depth - 2; i >= 0; i--) {
+	if (option->isAmericanOption()) {
+		for (int i = depth - 1; i >= 0; i--) {//go back up the tree by calculating the value of the option at each node
 			for (int j = 0; j <= i; j++) {
-				double continuous_value = (q * tree.getNode(i + 1, j + 1) + (1 - q) * tree.getNode(i + 1, j)) / (1 + interest_rate); 
-				double intrinsic_value = (*option).payoff(asset_price * power(1 + up, j) * power(1 + down, i - j));
+				double continuous_value = (q * tree.getNode(i + 1, j + 1) + (1 - q) * tree.getNode(i + 1, j)) / (1 + interest_rate);
+				double intrinsic_value = option->payoff(asset_price * power(1 + up, j) * power(1 + down, i - j));
 				tree.setNode(i, j, std::max(continuous_value, intrinsic_value));
+				// Set the exercise policy for American options
 				tree_bool.setNode(i, j, continuous_value <= intrinsic_value);
 			}
 		}
 	}
 	else {
-		for (int i = depth - 2; i >= 0; i--) {
+		for (int i = depth - 1; i >= 0; i--) {//go back up the tree by calculating the value of the option at each node
 			for (int j = 0; j <= i; j++) {
 				tree.setNode(i, j, (q * tree.getNode(i + 1, j + 1) + (1 - q) * tree.getNode(i + 1, j)) / (1 + interest_rate));
 			}
@@ -78,29 +85,45 @@ void CRRPricer::compute() {
 	}
 	//tree.display();
 }
-
+  
 double CRRPricer::get(int i, int j) {
+	if (j < 0 || i < 0 || j > depth || i > depth) {
+		throw std::out_of_range("Invalid tree indices");
+	}
 	return tree.getNode(i, j);
 }
 
-
 double CRRPricer::operator()(bool closed_form) {
-	if (closed_form == false) {
+	if (!closed_form)
+	{
 		compute();
+		// We return the H(0,0) already compute with the standard method
 		return get(0, 0);
 	}
-	else {
+	//closed-form formula for option pricing
+	else
+	{
+		// Closed form for European options only
+		if (option->isAmericanOption()) {
+			throw std::invalid_argument("Closed formula not available for American options");
+		}
 		double q = (interest_rate - down) / (up - down);
 		double sum = 0;
+		// The binomial coefficient is equal to 1 at the beginning because at i = 0 it is N!/N!
 		double binomial_coefficient = 1;
 		for (int i = 0; i < depth; i++) {
-			sum += binomial_coefficient*power(q,i)*power(1-q,depth-i-1)*tree.getNode(depth-1,i);
+			//We will use the formula provide and we have : tree.getNode(depth-1,i) = H(N,i) 
+			sum += binomial_coefficient * power(q, i) * power(1 - q, depth - i - 1) * tree.getNode(depth - 1, i);
+			//binomial_coefficient = N!/(i!(N − i)!)
 			binomial_coefficient = binomial_coefficient * (depth - i - 1) / (i + 1);
 		}
-		return (1 / power(1 + interest_rate, depth-1)) * sum;
+		return (1 / power(1 + interest_rate, depth - 1)) * sum;
 	}
 }
 
 bool CRRPricer::getExercise(int i, int j) {
+	if (j < 0 || i < 0 || j > depth || i > depth) {
+		throw std::out_of_range("Invalid tree indices");
+	}
 	return tree_bool.getNode(i, j);
 }
